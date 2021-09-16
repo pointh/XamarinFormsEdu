b1360fb56cf99ffac4730c04cb7aaf952a7a00ab
Pavel Svec <pavel.svec@itcrawl.cz>
2021-09-16 16:22:50 +0200

    Add project files.

---
+++ App1/App1/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace App1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            Debug.WriteLine(MethodBase.GetCurrentMethod().Name, "APP");
+        }
+
+        protected override void OnSleep()
+        {
+            Debug.WriteLine(MethodBase.GetCurrentMethod().Name);
+        }
+
+        protected override void OnResume()
+        {
+            Debug.WriteLine(MethodBase.GetCurrentMethod().Name);
+        }
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OK_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Upozornění", MethodBase.GetCurrentMethod().Name, "Zpět");
+        }
+
+        private void AppInfo_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Upozornění", Application.Current.Properties["LastMainPageAppeared"].ToString(), "Zpět");
+        }
+
+        private void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"MainPage appearing at {DateTime.Now}");
+            if (Application.Current.Properties.ContainsKey("LastMainPageAppeared") == false)
+                Application.Current.Properties.Add("LastMainPageAppeared", DateTime.Now);
+            else
+                Debug.WriteLine($"MainPage first time appeared at" +
+                    $" {Application.Current.Properties["LastMainPageAppeared"].ToString()}, now is {DateTime.Now}");
+        }
+
+        private void ContentPage_Disappearing(object sender, EventArgs e)
+        {
+            Debug.WriteLine("MainPage disappearing");
+
+        }
+
+        private void Resources_Clicked(object sender, EventArgs e)
+        {
+            Debug.WriteLine($"Resources: {string.Join(",", Application.Current.Resources.Keys)}");
+
+            // Dynamicky generované tlačítko
+            // MainStackLayout je StackLayout pojmenovaný v XAML filu.
+            Entry entry = new Entry() { Text = "Dynamicky generované tlačítko" };
+            MainStackLayout.Children.Add(entry);
+
+        }
+    }
+}
