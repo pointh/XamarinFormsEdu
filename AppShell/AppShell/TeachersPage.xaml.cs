9fb0a065d3d59c2916bd278bbf2991c78d5b7929
Pavel Svec <pavel.svec@itcrawl.cz>
2021-10-20 13:40:25 +0200

    ListView for Teachers + add empty teacher

--- AppShell/AppShell/TeachersPage.xaml.cs
+++ AppShell/AppShell/TeachersPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,34 @@ using Xamarin.Forms;
 
 namespace AppShell
 {
-    public partial class TeachersPage : ContentPage
+    public class Teacher
     {
+        public string Degree { get; set; }
+        public string Name { get; set; }
+    }
+
+    public partial class TeachersPage : ContentPage, INotifyPropertyChanged
+    {
+        public ObservableCollection<Teacher> TeachersList { get; set; }
         public TeachersPage()
         {
             InitializeComponent();
+            TeachersList = new ObservableCollection<Teacher>(
+                new Teacher[]
+                {
+                    new Teacher {Degree="Ing.", Name="R. Sáblík" },
+                    new Teacher {Degree="Mgr.", Name="Z. Nechanický" },
+                    new Teacher {Degree="Ing.", Name="Y. Scharnaglová" },
+                    new Teacher {Degree="Ing.", Name="P. Švec" },
+                });
+            BindingContext = this;
+        }
+
+        private void AddButton_Clicked(object sender, EventArgs e)
+        {
+            TeachersList.Add(new Teacher { Degree = "NoTitle", Name = "NoName" });
+            //TeachersListView.ItemsSource = null;
+            //TeachersListView.ItemsSource = this.TeachersList;
         }
     }
 }
